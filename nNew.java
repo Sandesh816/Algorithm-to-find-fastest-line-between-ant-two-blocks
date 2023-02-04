9a936a2182cf676167d03e7f49a0ac91e8dae6e3
Sandesh Ghimire <104772849+Sandesh816@users.noreply.github.com>
2023-02-04 15:38:51 -0500

    Update nNew.java
    Each square in a grid represents a co-ordinate axis; the top-left square will have co-ordinates (50,50) and the bottom-right square will have co-ordinates (650, 650). For input arguments, user needs to put it in the form of 50s. Then, the algorithm will search for the fastest route to go from point A to point B while igniting the least number of squares possible.

--- nNew.java
+++ nNew.java
@@ -1,15 +1,18 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class nNew extends JPanel {
-    private static int Width=500;
-    private static int Height=500;
+    private static int Width=700;
+    private static int Height=700;
     public nNew(){
         this.setPreferredSize(new Dimension(Width, Height));
     }
     @Override
-    public void paintComponent(Graphics g){
+    public void paintComponent(Graphics G){
         // Painting here
+        Graphics2D g = (Graphics2D) G;
         g.setColor(Color.black);
         int x=0, y=0;
         for (int i=0; i<8; i++){
@@ -21,8 +24,45 @@ public class nNew extends JPanel {
             g.drawLine(x,y,x,Height );
             x+=Width/7;
         }
-    }
 
+        System.out.println("Please input the starting X-point of your box (Must be in 50s): ");
+        Scanner X1 = new Scanner(System.in);
+        int x1 = X1.nextInt();
+        System.out.println("Please input the starting Y-point of your box (Must be in 50s): ");
+        Scanner Y1 = new Scanner(System.in);
+        int y1 = Y1.nextInt();
+        System.out.println("Please input the final X-point of your box (Must be in 50s): ");
+        Scanner X2 = new Scanner(System.in);
+        int x2 = X2.nextInt();
+        System.out.println("Please input the final Y-point of your box (Must be in 50s): ");
+        Scanner Y2 = new Scanner(System.in);
+        int y2 = Y2.nextInt();
+
+        paintBoxes(x1,y1,g);
+        paintBoxes(x2,y2,g);
+
+        while (x1!=x2 || y1!=y2){
+            x1 = values(x1,y1,x2,y2)[0];
+            y1= values(x1,y1,x2,y2)[1];
+            paintBoxes(x1,y1,g);
+        }
+    }
+    public void paintBoxes(int x, int y, Graphics2D g){
+        g.setColor(Color.blue);
+        g.fillRect(x-50, y-50, 100,100);
+    }
+    public static int[] values(int x1, int y1, int x2, int y2){
+        int [] array = new int[2];
+        if (x1==x2){array[0]=x1;}
+        if (x2>x1){array[0]=x1+100;}
+        if (x2<x1){array[0]=x1-100;}
+        System.out.println("Done with X");
+        if (y1==y2){array[1]=y1;}
+        if (y2>y1){array[1]=y1+100;}
+        if (y2<y1){array[1]=y1-100;}
+        System.out.println("Done with Y");
+        return array;
+    }
     public static void main(String[] args) {
         JFrame frame= new JFrame("nNew");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
